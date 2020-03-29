36f66180e1ab0f062020bd6a56d673e3e56859f1
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-03-28 20:37:17 -0400

    Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,17 @@
 import React,{Component} from 'react';
 
+//setting up a loop through all grid spaces 
+const grid = ['1','2','3','4','5']
+
+// A more dynamic set up 
+// Main file
+// return (
+//   <Grid2 />
+//   <Grid3 />
+//   <Grid4 />
+//   <Grid5 />
+// )
+
 
   
   
