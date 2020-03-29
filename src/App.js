956fb43eede7663fee38de7e944e3c143f78305a
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-03-28 20:24:23 -0400

    imports files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import Main from './components/Main'
+import Curtain from './components/Curtain'
+
+
 
 function App() {
   return (
