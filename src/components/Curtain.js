0cd18478c081fd3c77ec68db2f7d705a6f6eb09e
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-03-28 20:27:00 -0400

    Component1

---
+++ src/components/Curtain.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Curtain(){
+    return (
+        <div className="cover">
+            <h1>Hello World</h1>
+          </div>
+           
+      
+    )
+    
+}
+
+
+
+export default Curtain;
