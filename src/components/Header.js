0606f07af52f9ee3833806b95e47d1443f8b962a
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-03-28 20:32:46 -0400

    Header Array

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,41 @@ const listNames = [
     'SUBSCRIBE'
 ]
 
+import React, { Component } from 'react';
+//setting navbar contents into a aray 
+const listNames = [
+    'Nautilus',
+    'ISSUES',
+    'TOPICS',
+    'BLOG',
+    'NEWSLETTER',
+    'FACEBOOK',
+    'TWITTER',
+    'LOGIN',
+    'SUBSCRIBE'
+]
 
+
+
+class Header extends Component {
+    render() {
+        return (
+            <ul className="nav">
+                {   
+            //mapping through the array and passing in name/content
+                    
+                }
+            </ul>
+        ) 
+    }
+}
+// links reapeat so i can make another component 
+
+
+
+
+
+export default Header;
 
 
 
