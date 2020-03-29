688d0587a11f42d93d4201d389d72f58c0b0681c
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-03-28 20:40:07 -0400

    Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,10 +11,25 @@ const grid = ['1','2','3','4','5']
 //   <Grid4 />
 //   <Grid5 />
 // )
+//setting up jsx variable
+ let g1s = <div className="top">
+  <h6 className="topText1">The Natural Spotlight</h6>
+  <h1 className="topTex2">WHY THE LAWS OF PHYSICS ARE INEVITABLE</h1>
+  <button className="topB"type="button">READ NOW</button> 
+  </div>
 
-
-  
-  
+// main function to export 
+// main function to export 
+function Main() {
+ 
+    return (
+      // grid container 
+      <div className="gridContainer">
+          <h1>{g1s}</h1>
+      </div>
+    );
+  }
+ 
 
 
 export default Main;
