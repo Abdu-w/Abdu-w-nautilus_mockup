f9e04316c887c83cfea2ec80b6c893ffabb1cdd7
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-03-28 20:28:08 -0400

    Component1

--- src/components/Curtain.js
+++ src/components/Curtain.js
@@ -6,7 +6,8 @@ function Curtain(){
             <div className='coverIssue'>ISSUE 079</div>
             <div className='covertitle'>Catalysts</div>
             <div className='coverSub'>Agents of Change</div>
-           
+            <div className='coverLink'><a href='#'>READ MORE</a></div>
+            <div><button className="coverButton"type="button">SEE FULL ISSUE</button></div>
           </div>
            
       
